2d6fda44a63924f57a03645beaee3f1e71177f90
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-09-23 15:43:23 +0100

    feat: update files

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //Sélection des éléments du DOM
-const buttonJePostule = document.querySelector(
-  ".header__left__button__postule"
-);
+const userName = document.querySelector("#name");
+const email = document.querySelector("#email");
+const startUpName = document.querySelector("#startup_name");
+const message = document.querySelector("message");
+const form = document.querySelector("form");
 
-buttonJePostule.addEventListener("click", goIntoForm);
+console.log(userName);
 
-function goIntoForm() {
-  //Codes ici
+const submitButton = document.getElementById("submit_button");
+
+submitButton.addEventListener("click", handleSubmit);
+
+function handleSubmit() {
+  form.reset();
 }
