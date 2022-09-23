66fd78b920c2a25ef472a7f42c68c3c0c06032e2
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-09-23 07:54:39 +0100

    feat: update js file

---
+++ script.js
@@ -0,0 +1,10 @@
+//Sélection des éléments du DOM
+const buttonJePostule = document.querySelector(
+  ".header__left__button__postule"
+);
+
+buttonJePostule.addEventListener("click", goIntoForm);
+
+function goIntoForm() {
+  //Codes ici
+}
