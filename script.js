be150aeeb9abe3e961a7edf1454e23d6f3759df3
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-09-26 18:04:54 +0100

    update files

--- script.js
+++ script.js
@@ -110,8 +110,8 @@ const form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
+  const status = document.getElementById("my-form-status");
+  const data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
@@ -121,7 +121,7 @@ async function handleSubmit(event) {
   })
     .then((response) => {
       if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
+        status.innerHTML = "Merci pour votre soumission!";
         form.reset();
       } else {
         response.json().then((data) => {
@@ -130,7 +130,8 @@ async function handleSubmit(event) {
               .map((error) => error["message"])
               .join(", ");
           } else {
-            status.innerHTML = "Oops! There was a problem submitting your form";
+            status.innerHTML =
+              "Oops! Il y a eu un problème lors de la soumission de votre formulaire";
           }
         });
       }
