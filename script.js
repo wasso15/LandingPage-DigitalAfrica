3d53e491ee4d002a2d6efbe0bbe9f672b9738dd9
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-09-27 10:09:11 +0100

    moving from var keyword to const keyword for variables declaration

--- script.js
+++ script.js
@@ -110,8 +110,8 @@ const form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
+  const status = document.getElementById("my-form-status");
+  const data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
